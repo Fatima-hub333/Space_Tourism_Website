cb77657eb86c6022bb9a862cc2a260566ccf5a2d
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-19 22:41:16 +0500

    navigation for medium sizes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-// when someone clicks the hamburger button
 navToggle.addEventListener("click", () => {
-    // if the nav is closed, open it
   const visiblity = nav.getAttribute("data-visible");
   if (visiblity === "false") {
     nav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
   } else {
     nav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
-    
-    // if the nav is open, close it
 })
