d11ea32a29119b595c1c54051ab6d9ed18e5e89a
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-22 21:08:05 +0500

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,6 +1,27 @@
-const tabLists = document.querySelector('[role="tablist"]');
-const tabs = tabLists.querySelector('[role="tab"]');
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
 
-tabLists.addEventListener('keydown', (e) => {
-  console.log(e.keyCode);
+let tabFocus = 0;
+
+tabList.addEventListener('keydown', (e) => {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+    
+    // change the tabindex of the current tab to -1
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
+    
+    // if the right key is pushed, move to the next tab on the right
+    if (e.keyCode === keydownRight) {
+        tabFocus++;
+    }
+    
+    // if the left key is pushed, move to the next tab on the left
+ if (e.keyCode === keydownLeft) {
+        tabFocus--;
+  }
+  
+  tabs[tabFocus].setAttribute("tabindex", -1);
+  tabs[tabFocus].focus;
 })
