9ae13a8cb64a8e072dd7aa60641562440e07eccd
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-22 21:58:35 +0500

    Hiding the previous tab

--- tabs.js
+++ tabs.js
@@ -1,27 +1,54 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-let tabFocus = 0;
+tabList.addEventListener('keydown', changeTabFocus);
 
-tabList.addEventListener('keydown', (e) => {
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
+
+let tabFocus = 0;
+function changeTabFocus(e) {
     const keydownLeft = 37;
     const keydownRight = 39;
     
-    // change the tabindex of the current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
     }
     
-    // if the right key is pushed, move to the next tab on the right
     if (e.keyCode === keydownRight) {
         tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
     }
     
-    // if the left key is pushed, move to the next tab on the left
- if (e.keyCode === keydownLeft) {
+    if (e.keyCode === keydownLeft) {
         tabFocus--;
-  }
-  
-  tabs[tabFocus].setAttribute("tabindex", -1);
-  tabs[tabFocus].focus;
-})
+        if (tabFocus < 0) {
+            tabFocus = tabs.length - 1;
+        }
+    }
+    
+    tabs[tabFocus].setAttribute("tabindex", 0);
+    tabs[tabFocus].focus();
+}
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+    
+    mainContainer
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach((panel) => panel.setAttribute("hidden", true));
+    
+    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    
+    
+    // console.log(mainContainer);
+}
