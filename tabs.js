ca934dcc2ad914355e174eac9200bcd8d6c3bb45
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-08-22 22:05:21 +0500

    changing the image

--- tabs.js
+++ tabs.js
@@ -39,6 +39,7 @@ function changeTabFocus(e) {
 function changeTabPanel(e) {
     const targetTab = e.target;
     const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
     
     const tabContainer = targetTab.parentNode;
     const mainContainer = tabContainer.parentNode;
@@ -49,6 +50,11 @@ function changeTabPanel(e) {
     
     mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
     
+    mainContainer
+        .querySelectorAll('picture')
+        .forEach((picture) => picture.setAttribute("hidden", true));
+        
+    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
     
     // console.log(mainContainer);
 }
